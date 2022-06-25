3013b6aa18e35afbec22c80ae56abcd662c3f8bf
Sammith1 <hsammith@gmail.com>
2022-06-25 20:00:58 +0530

    added sort functionlity

--- Backend/Controller/Noticebord_cont.js
+++ Backend/Controller/Noticebord_cont.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Notice = require("../Module/notice_module");
 router.get("/", async (req, res) => {
   try {
-    const notice = await Notice.find().lean().exec();
+    const notice = await Notice.find().sort({ _id: -1 }).lean().exec();
     return res.status(201).send(notice);
   } catch (er) {
     return res.status(501).send({ Message: er.message });
